0452f4946bd005d62fe15842a3eb0904916713cb
cc <cc_home@qq.com>
2013-07-03 00:14:32 +0800

    draw feng shui map init

---
+++ DrawMapFengShui/DrawMapFengShui.WinApp/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DrawMapFengShui.WinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(153, 178);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnDraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
+
---
+++ DrawMapFengShui/DrawMapFengShui.WinApp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DrawMapFengShui.WinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("User32.dll")]
+        public extern static IntPtr GetDC(System.IntPtr hWnd);
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            var brush = new SolidBrush(Color.Blue);
+            Pen penBlue = new Pen(Color.Blue);
+
+            IntPtr DesktopHandle = GetDC(IntPtr.Zero);
+            Graphics g = System.Drawing.Graphics.FromHdc(DesktopHandle);
+            g.FillRectangle(new SolidBrush(Color.Red), 0, 0, 100, 100);
+
+            PointF pb = new PointF(0f, 0f);
+
+            PointF pe = new PointF(100f, 200f);
+            g.DrawLine(penBlue, pb, pe);
+
+        }
+    }
+}
