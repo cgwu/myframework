0b84f324cc47f7941239a7f1f8b55f201a6785fd
cg <superwcg@gmail.com>
2013-07-27 11:50:11 +0800

    动态网址的WebService

---
+++ WebServiceDynamicURLDemo/WebServiceDynamicURLDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebServiceDynamicURLDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //带网址的WebService
+            PdaWebSrv.PdaWebSrvSoapClient pda = new PdaWebSrv.PdaWebSrvSoapClient("PdaWebSrvSoap", "http://xxx.com/PdaWebSrv.asmx");
+
+            string member = "jl000";
+            string password = "000000";
+
+            string errorMsg = "";
+            int agent_id = 0;
+            int iRet = pda.ETLoginNoEnc(ref errorMsg, member, password, ref agent_id);
+
+            Console.WriteLine(iRet);
+
+            Console.WriteLine("done");
+            Console.ReadKey();
+        }
+    }
+}
